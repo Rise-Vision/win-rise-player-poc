c0c5fd20de6ba2c077c2a7a9639f22d0e28ba656
Santiago <santiago.noguez@risevision.com>
2021-01-22 12:36:04 -0600

    add squirrel installer

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CefSharp;
 using CefSharp.Wpf;
+using Squirrel;
 using System.Windows;
 
 namespace WinRisePlayerPOC
@@ -12,5 +13,16 @@ namespace WinRisePlayerPOC
 
             Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
         }
+
+        protected override async void OnStartup(StartupEventArgs args)
+        {
+            base.OnStartup(args);
+
+            using (var updateManager = new UpdateManager("C:\\Users\\Santiago\\Source\\Repos\\WinRisePlayerPOC\\Releases"))
+            {
+                await updateManager.UpdateApp();
+            }
+        }
+
     }
 }
