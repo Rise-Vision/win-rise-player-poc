54201df7e0ca5d7622ffd12c7c26a6f817749d04
Santiago <santiago.noguez@risevision.com>
2021-01-22 10:21:57 -0600

    add basic version

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System.Windows;
+
+namespace WinRisePlayerPOC
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            var settings = new CefSettings();
+
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+        }
+    }
+}
