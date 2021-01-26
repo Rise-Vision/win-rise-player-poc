3659913fa29b47b7d17ee1ecf247405de0e58625
Santiago <santiago.noguez@risevision.com>
2021-01-26 11:46:30 -0600

    Net 461 configuration

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using Squirrel;
+using System.Windows;
+
+namespace WinPOC461
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            var settings = new CefSettings();
+
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+        }
+
+        protected override async void OnStartup(StartupEventArgs args)
+        {
+            base.OnStartup(args);
+
+            using (var updateManager = new UpdateManager("https://storage.googleapis.com/risevision-display-notifications/4TKGKVY5GQ5C"))
+            {
+                await updateManager.UpdateApp();
+            }
+        }
+    }
+}
